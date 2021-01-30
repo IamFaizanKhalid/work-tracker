a923d68bd9f649c7a6a64165341d9ad52370b4cc
Faizan Khalid <IamFaizanKhalid@gmail.com>
2021-01-30 16:35:23 +0500

    Refactored Record

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/IamFaizanKhalid/work-tracker/tracker"
 	"image/png"
@@ -13,9 +15,11 @@ import (
 const DURATION = 10
 
 var WorkDir string
-var CaptureDir string
+var CurrentDir string
 
 type Record struct {
+	WeeklyRecord    int
+	DailyRecord     int
 	Timestamp       time.Time
 	ActiveWindow    string
 	KeyboardStrokes int
@@ -30,9 +34,9 @@ func main() {
 	}
 
 	WorkDir = HomeDirectory + "/.work-tracker"
-	CaptureDir = WorkDir + "/captures"
+	CurrentDir = WorkDir + "/" + time.Now().Format("20060102")
 
-	err = os.MkdirAll(CaptureDir, os.ModePerm)
+	err = os.MkdirAll(CurrentDir, os.ModePerm)
 	if err != nil {
 		log.Fatalf("Error creating working directory: %v", err)
 	}
@@ -41,31 +45,25 @@ func main() {
 }
 
 func startTracking() {
+	var record Record
+
 	minutesPassed := 0
-	captures := 0
 	captureAfter := 1 + rand.Int()%DURATION
-	ticker := time.NewTicker(time.Duration(captureAfter) * time.Minute)
+	ticker := time.NewTicker(time.Duration(captureAfter) * time.Second)
 
 	for range ticker.C {
-		captures += 1
+		record.DailyRecord += 1
+		record.WeeklyRecord += 1
 		minutesPassed += captureAfter
 		captureAfter = (1 + rand.Int()%DURATION) + (DURATION - captureAfter)
-		ticker.Reset(time.Duration(captureAfter) * time.Minute)
+		ticker.Reset(time.Duration(captureAfter) * time.Second)
 
-		timestamp := time.Now()
-		saveScreenshot(timestamp)
+		record.Timestamp = time.Now()
+		saveScreenshot(record.Timestamp)
+		record.ActiveWindow = tracker.GetActiveWindowName()
 
-		x := Record{
-			Timestamp:       timestamp,
-			ActiveWindow:    tracker.GetActiveWindowName(),
-			KeyboardStrokes: 0,
-			MouseStrokes:    0,
-			ActivityLevel:   0,
-		}
-
-		fmt.Printf("Logged today:\t%v\t\t\tLogged this week:\t%[1]v\n", getTimeLogged(captures))
-		fmt.Printf("Active window:\t%s\n", x.ActiveWindow)
-		fmt.Println()
+		record.log()
+		record.print()
 	}
 }
 
@@ -75,7 +73,7 @@ func getTimeLogged(captures int) string {
 }
 
 func saveScreenshot(timestamp time.Time) {
-	fileName := fmt.Sprintf(CaptureDir+"/capture_%s.png", timestamp.Format("20060102150405"))
+	fileName := fmt.Sprintf(CurrentDir+"/%s.png", timestamp.Format("150405"))
 
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -83,8 +81,47 @@ func saveScreenshot(timestamp time.Time) {
 	}
 	defer file.Close()
 
-	err = png.Encode(file, tracker.GetScreenShot())
+	encoder := &png.Encoder{
+		CompressionLevel: png.BestCompression,
+	}
+
+	err = encoder.Encode(file, tracker.GetScreenShot())
 	if err != nil {
 		log.Printf("Error writing image: %v", err)
 	}
+}
+
+func (r *Record) log() {
+	b, err := json.Marshal(r)
+	if err != nil {
+		log.Printf("Error marshalling record: %v", err)
+	}
+
+	buffer := new(bytes.Buffer)
+	err = json.Compact(buffer, b)
+	if err != nil {
+		log.Printf("Error compacting json record: %v", err)
+	}
+	buffer.WriteByte('\n')
+
+	file, err := os.OpenFile(CurrentDir+"/logs", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0660)
+	if err != nil {
+		log.Printf("Error opening log file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(buffer.Bytes())
+	if err != nil {
+		log.Printf("Error writing log file: %v", err)
+	}
+}
+
+func (r *Record) print() {
+	fmt.Printf("%s\n--\n", r.Timestamp.Format("Monday, 02 Jan 2006 15:04:05 MST"))
+	fmt.Printf("> Logged this week:\t%v\n", getTimeLogged(r.WeeklyRecord))
+	fmt.Printf("> Logged today:\t\t%v\n", getTimeLogged(r.DailyRecord))
+	fmt.Printf("> Active window:\t%s\n", r.ActiveWindow)
+	fmt.Printf("> Activity level:\t%d\n", r.ActivityLevel)
+	fmt.Printf("> Events:\t\t%d keyboard, %d mouse\n", r.KeyboardStrokes, r.MouseStrokes)
+	fmt.Printf("------------------------------\n\n")
 }
